8ac68bb4f38283bb4e0675a0a4e3eb9f9993c41e
Amy Carney <digilou@users.noreply.github.com>
2016-12-26 16:23:42 -0900

    Wrestling with API
    Still messing with the JS to get the data file. Reading through jQuery docs now....

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
 // MAAS API http://marsweather.ingenology.com/
 
-$(document).ready(function() {
-  $.getJSON("http://marsweather.ingenology.com/v1/latest/", function(a) {
-    $("#forecast").html("<p>The weather on Mars is currently<span id='temp'" + a[0].max_temp_fahrenheit + "</span></p><p>Today it's <span id='atmosphere'" + a[0].atmo_opacity + "</span></p>")
+// pull in data as soon as document loads
+(function() {
+  var maasAPI = "http://marsweather.ingenology.com/v1/latest/";
+  $.getJSON(maasAPI, {format:"jsonp"});
+    .done(function(data) {
+      var temperature = report.max_temp_fahrenheit.val();
+      var conditions = report.atmo_opacity.val();
+      $.each(data.items, function(i, item){
+        $("#forecast").html("<p>The weather on Mars is currently <span id='temp'" + temperature + "</span></p><p>Today it's <span id='atmosphere'" + conditions + "</span></p>")
+      });
     });
-    // $.ajaxSetup({cache: false});  
-});
+    // $.ajaxSetup({cache: false});
+
+}); //end of entire function
+
+// http://api.jquery.com/jquery.getjson/
+
+// Martian Chronicles: black river, discal forest, wilder town, second try, spender hill, iron town, grain villa, detroit moss town (cemetary), father peregrine, usher
+// The Martian: Mark Whitney
