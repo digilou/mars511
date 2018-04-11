69d0d63d64ea975b69b9781352a61239bbeab011
Amy Carney <click2carney@yahoo.com>
2018-04-11 07:42:28 -0800

    rearrange files; register service worker; update server; add skeleton files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+// import HeaderContainer from './components/Header'
+// import Search from './components/Search'
+// import Container from './components/Places'
+// import Wiki from './components/Description'
+// import Footer from './components/Footer'
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Hello World</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
