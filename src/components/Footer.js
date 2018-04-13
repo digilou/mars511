8b2f52a9d25c7427f0ae19af5b1b62310f0f25af
Amy Carney <click2carney@yahoo.com>
2018-04-13 06:51:09 -0800

    add bkgd img and mixin; update readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class Footer extends Component {
+export default class Footer extends Component {
   render() {
     return (
       <footer>
@@ -8,19 +8,19 @@ class Footer extends Component {
         <p>Copyright &copy;2018 | Amy Carney</p>
         <ul>
         <li><a href="https://github.com/digilou" aria-label="Follow me on Github" title="Follow me on Github">
-          <span className="fa fa-github fa-2x" aria-hidden></span>
+          <span className="fa fa-github fa-3x" aria-hidden></span>
           </a>
         </li>
         <li><a href="https://www.linkedin.com/in/carneyamy" aria-label="Connect with me on LinkedIn" title="Connect with me on LinkedIn">
-          <span className="fa fa-linkedin fa-2x" aria-hidden></span>
+          <span className="fa fa-linkedin fa-3x" aria-hidden></span>
           </a>
         </li>
         <li><a href="https://carneydevelopoit.wordpress.com/" aria-label="Follow me on WordPress" title="Follow me on WordPress">
-          <span className="fa fa-wordpress fa-2x" aria-hidden></span>
+          <span className="fa fa-wordpress fa-3x" aria-hidden></span>
           </a>
         </li>
         <li><a href="https://twitter.com/click2carney" aria-label="Follow me on Twitter" title="Follow me on Twitter">
-          <span className="fa fa-twitter fa-2x" aria-hidden></span>
+          <span className="fa fa-twitter fa-3x" aria-hidden></span>
           </a>
         </li>
         </ul>
@@ -28,5 +28,3 @@ class Footer extends Component {
     );
   }
 }
-
-export default Footer;
